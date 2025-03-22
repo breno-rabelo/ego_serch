a20653c8d12c1e700be89f3aaa2fe8794812a484
Breno Rabelo do Nascimento <127445329+breno-rabelo@users.noreply.github.com>
2025-03-22 20:32:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@
 # A API e limitada a 20
 import tweepy
 
-API_KEY = 'HYNuKr21kbfk1fcbXc9YsBKun'
-API_SECRET = 'NXSrwiJetdCHbbL6XBsZLAHS227BuJz8f7PCx65LUe8rNyjbls'
-BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAG1u0AEAAAAAZ6byoQlApkZS%2FDmH0TfRC3%2FEkz8%3Dab2wf0SuHjX5Vx3Y523s7VgDt3jYMKAZrdr7MrUUqL8eHtx4sv'
+API_KEY = 'API_KEY'
+API_SECRET = 'API_SECRET'
+BEARER_TOKEN = 'BEARER_TOKEN'
 
 # Autenticando a chave API com Bearer Token
 client = tweepy.Client(bearer_token=BEARER_TOKEN)
